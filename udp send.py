aceb5a4c54791b5b0afcd05416e19b59abb2e73e
Majid Motallebikashani <chakmeshma@gmail.com>
2023-01-10 00:11:08 +0330

    udp send

---
+++ udp send.py
@@ -0,0 +1,44 @@
+import socket
+import time
+
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
+msgFromServer = "Hello UDP Client"
+
+bytesToSend = str.encode(msgFromServer)
+
+# Create a datagram socket
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+address = ('45.35.13.55', 1666)
+
+while(True):
+    UDPServerSocket.sendto(bytesToSend, address)
+    time.sleep(0.3)
+
+# Bind to address and ip
+
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+
+while (True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+    message = bytesAddressPair[0]
+
+    address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+
+    UDPServerSocket.sendto(bytesToSend, address)
