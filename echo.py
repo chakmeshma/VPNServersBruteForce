a9f12b20ddfaaf07faac4996704cd0ddfe347ff1
Majid Motallebikashani <chakmeshma@gmail.com>
2023-01-15 03:28:55 +0330

    Echo good

--- echo.py
+++ echo.py
@@ -1,21 +1,41 @@
 import socket
+import sys
 
-HOST = '0.0.0.0'
-PORT = 9999
-
-def start_echo():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                conn.sendall(data)
+HOST = ''  # Symbolic name meaning all available interfaces
+PORT = 9999  # Arbitrary non-privileged port
 
 
-while True:
+def echo_process():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+
     try:
-        start_echo()
+        s.bind((HOST, PORT))
+    except:
+        sys.exit()
+
+    print('Socket bind complete')
+
+    s.listen(1000)
+
+    print('Socket now listening')
+
+    # now keep talking with the client
+    while 1:
+        # wait to accept a connection - blocking call
+        conn, addr = s.accept()
+        print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+        while True:
+            data = conn.recv(1024)
+            reply = 'OK...' + data.decode()
+            if not data:
+                break
+            conn.sendall(reply.encode())
+
+
+while (True):
+    try:
+        echo_process()
     except:
         pass
