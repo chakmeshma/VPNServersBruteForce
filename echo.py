9799bd14e72f96bce7f06031becb82f7bc8b4bde
Majid Motallebikashani <chakmeshma@gmail.com>
2023-01-15 03:02:59 +0330

    TCP Echo Server2

--- echo.py
+++ echo.py
@@ -3,15 +3,8 @@ import socket
 HOST = '0.0.0.0'
 PORT = 9999
 
-last_socket = None
-
-
 def start_echo():
-    global last_socket
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        last_socket = s
-
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
@@ -24,6 +17,5 @@ def start_echo():
 while True:
     try:
         start_echo()
-        last_socket.close()
     except:
         pass
