d866011e5c9f5d6f1add7538142bece9dbf4fe76
Majid Motallebikashani <chakmeshma@gmail.com>
2023-05-26 18:50:01 +0330

    all new

--- scanner threadpool2.py
+++ scanner threadpool2.py
@@ -5,10 +5,10 @@ from timeit import default_timer as timer
 import urllib3
 import graceful_close
 
-response_timeout = 100
-connect_timeout = 100
+response_timeout = 30
+connect_timeout = 30
 
-target_address = 'www.facebook.com:443'
+target_address = 'www.google.com:443'
 
 
 def check_connect_and_connetverb(host_address: str, host_port: int, opens: set):
@@ -53,6 +53,7 @@ def check_connect_and_connetverb(host_address: str, host_port: int, opens: set):
                     raise TimeoutError()
 
         opens.add("{}:{}".format(host_address, host_port))
+        # if host_port == 443:
         print("{} {}".format(host_address, host_port))
     except TimeoutError as e:
         pass
@@ -76,7 +77,14 @@ def fetch_them(targets: set, thread_pool_size: int):
 
 
 def parse_feed(feed_str: str):
-    feed_list = feed_str.split('\n')
+    if feed_str.find('\n\a') != -1:
+        feed_delimieter = '\n\a'
+    elif feed_str.find('\n') != -1:
+        feed_delimieter = '\n'
+    else:
+        raise Exception()
+
+    feed_list = feed_str.split(feed_delimieter)
 
     targets = set()
 
@@ -104,32 +112,37 @@ def get_targets_file(file_name: str):
     return parse_feed(feed_str)
 
 
-def get_targets_file_json(file_name: str):
-    with open(file_name, 'rb') as feedfile:
-        feed_bin = feedfile.read()
-
-    feed_str = feed_bin.decode('utf-8')
-
-    zzz = json.loads(feed_str)['data']
-
-    targets = set()
-
-    for i in zzz:
-        targets.add((i['ip'], int(i['port'])))
-
-    return targets
+# def get_targets_file_json(file_name: str):
+#     with open(file_name, 'rb') as feedfile:
+#         feed_bin = feedfile.read()
+#
+#     feed_str = feed_bin.decode('utf-8')
+#
+#     zzz = json.loads(feed_str)['data']
+#
+#     targets = set()
+#
+#     for i in zzz:
+#         targets.add((i['ip'], int(i['port'])))
+#
+#     return targets
 
 
 with open('requestpdu_sexhost.bin', 'rb') as requestbinfile:
     requestbin = requestbinfile.read()
 requestbin = requestbin.replace(b'sexhost', target_address.encode('ascii'))
+# requeststr = requestbin.decode('ascii')
 
 targets = set()
-targets.update(get_targets_file_json('sex.json'))
-targets.update(get_targets_file('feed.txt'))
-targets.update(
-    get_targets_url('https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt'))
+# targets.update(get_targets_file_json('sex.json'))
+# targets.update(get_targets_file('feed3.txt'))
+# targets.update(get_targets_url('https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt'))
+targets.update(get_targets_url('https://raw.githubusercontent.com/aslisk/proxyhttps/main/https.txt'))
+targets.update(get_targets_url('https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt'))
+targets.update(get_targets_url('https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt'))
+targets.update(get_targets_url('https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt'))
+targets.update(get_targets_url('https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt'))
 
-opens = fetch_them(targets, 200)
+opens = fetch_them(targets, 100)
 
 print("Found {}".format(len(opens)))
